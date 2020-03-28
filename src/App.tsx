4078be1ca060dbbf25af83dcfdcb44bbdc75274c
Jerry Tabernero <tabernerojerry@gmail.com>
2020-03-28 17:21:27 +0800

    xstate communicate between state machines in different components

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,38 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useMachine } from '@xstate/react';
 
-import { fetchMachine } from './machines/fetch-machine';
 import { matchingMachine } from './machines/matching-machine';
+import { List } from './List';
 
 const apiUsers = 'https://jsonplaceholder.typicode.com/users';
 const apiPosts = 'https://jsonplaceholder.typicode.com/posts';
 
 export const App = () => {
-  const [fetchUsers, sendToUsersMachine] = useMachine(fetchMachine, {
-    services: {
-      fetchData: (ctx, event) =>
-        fetch(apiUsers)
-          .then(r => r.json())
-          .then(data => data),
-    },
-  });
-
-  const [fetchPosts, sendToPostsMachine] = useMachine(fetchMachine, {
-    services: {
-      fetchData: (ctx, event) =>
-        fetch(apiPosts)
-          .then(r => r.json())
-          .then(data => data),
-    },
-  });
-
   const [matchingState, sendToMatchingMachine] = useMachine(matchingMachine);
-
-  useEffect(() => {
-    sendToPostsMachine({ type: 'FETCH' });
-    sendToUsersMachine({ type: 'FETCH' });
-  }, [sendToPostsMachine, sendToUsersMachine]);
 
   return (
     <div className="App">
       {matchingState.matches('answering') && (
         <>
-          <button onClick={() => sendToUsersMachine({ type: 'FETCH' })}>
-            Fecth
-          </button>
-
-          {fetchUsers.matches('pending') && <p>Loading...</p>}
-
-          {fetchUsers.matches('success.withData') && (
-            <ul>
-              {fetchUsers.context.results.map((user: any) => (
-                <li key={user.id}>
-                  <button
-                    style={{
-                      backgroundColor:
-                        matchingState.context.topSelectedItem === user
-                          ? 'blue'
-                          : '',
-                    }}
-                    onClick={() =>
-                      sendToMatchingMachine({
-                        type: 'SELECT_TOP',
-                        selectedItem: user,
-                      })
-                    }
-                  >
-                    {user.name}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          )}
-
-          {fetchUsers.matches('success.emptyData') && <p>No data available.</p>}
-
-          {fetchUsers.matches('failed') && (
-            <p>Error: {fetchUsers.context.message}</p>
-          )}
+          <List
+            keyName="name"
+            fetchData={() => fetch(apiUsers).then(r => r.json())}
+            selectedItem={matchingState.context.topSelectedItem}
+            onSelection={selectedItem =>
+              sendToMatchingMachine({ type: 'SELECT_TOP', selectedItem })
+            }
+          ></List>
 
           <hr />
 
-          <button onClick={() => sendToPostsMachine({ type: 'FETCH' })}>
-            Fecth
-          </button>
-
-          {fetchPosts.matches('pending') && <p>Loading...</p>}
-
-          {fetchPosts.matches('success.withData') && (
-            <ul>
-              {fetchPosts.context.results.map((post: any) => (
-                <li key={post.id}>
-                  <button
-                    style={{
-                      backgroundColor:
-                        matchingState.context.bottomSelectedItem === post
-                          ? 'blue'
-                          : '',
-                    }}
-                    onClick={() =>
-                      sendToMatchingMachine({
-                        type: 'SELECT_BOTTOM',
-                        selectedItem: post,
-                      })
-                    }
-                  >
-                    {post.title}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          )}
-
-          {fetchPosts.matches('success.emptyData') && <p>No data available.</p>}
-
-          {fetchPosts.matches('failed') && (
-            <p>Error: {fetchPosts.context.message}</p>
-          )}
+          <List
+            keyName="title"
+            fetchData={() => fetch(apiPosts).then(r => r.json())}
+            selectedItem={matchingState.context.bottomSelectedItem}
+            onSelection={selectedItem =>
+              sendToMatchingMachine({ type: 'SELECT_BOTTOM', selectedItem })
+            }
+          ></List>
         </>
       )}
 
---
+++ src/List.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import { useMachine } from '@xstate/react';
+import { fetchMachine } from './machines/fetch-machine';
+
+interface IProps {
+  keyName: string;
+  fetchData: () => Promise<{ results: any[] }>;
+  selectedItem: any;
+  onSelection: (item: any) => void;
+}
+
+export const List: React.FC<IProps> = ({
+  keyName,
+  fetchData,
+  selectedItem,
+  onSelection,
+}) => {
+  const [fetchDataState, sendToDataMachine] = useMachine(fetchMachine, {
+    services: {
+      fetchData: () => fetchData().then(results => results),
+    },
+  });
+
+  useEffect(() => {
+    sendToDataMachine({ type: 'FETCH' });
+  }, [sendToDataMachine]);
+
+  return (
+    <>
+      <button onClick={() => sendToDataMachine({ type: 'FETCH' })}>
+        Fecth
+      </button>
+
+      {fetchDataState.matches('pending') && <p>Loading...</p>}
+
+      {fetchDataState.matches('success.withData') && (
+        <ul>
+          {fetchDataState.context.results.map((item: any) => (
+            <li key={item.id}>
+              <button
+                className={selectedItem === item ? 'selected' : ''}
+                onClick={() => onSelection(item)}
+              >
+                {item[keyName]}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {fetchDataState.matches('success.emptyData') && <p>No data available.</p>}
+
+      {fetchDataState.matches('failed') && (
+        <p>Error: {fetchDataState.context.message}</p>
+      )}
+    </>
+  );
+};
